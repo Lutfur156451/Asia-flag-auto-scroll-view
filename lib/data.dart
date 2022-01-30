db677ccacc2575d17904fc1ed6d62cf835839f7f
User <rifatal850@gmail.com>
2022-01-30 20:10:12 +0600

    Asaia commit

---
+++ lib/data.dart
@@ -0,0 +1,37 @@
+List movies1 = [
+
+  'aus.png',
+  'cd.png',
+  'images.png',
+  'bg1.png',
+
+
+
+   'mg.png',
+  'bg1.png',
+
+];
+
+List movies2 = [
+
+
+
+  'cnp.png',
+  'mgp.png',
+  'sk2.jfif',
+  'ausp.jfif',
+  'inp.jfif',
+  'skp.jfif',
+];
+
+List movies3 = [
+  'cd.png',
+  'mg.png',
+  'bg1.png',
+  'aus.png',
+  'images.png',
+  'bg1.png',
+
+
+];
+
